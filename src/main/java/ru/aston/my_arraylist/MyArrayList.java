d9ee40bb6279a9c905a213a4a99572f67876366b
SemenenkoEugene <gks-08@mail.ru>
2023-12-16 16:47:41 +0300

    feat: добавил метод add

---
+++ src/main/java/ru/aston/my_arraylist/MyArrayList.java
@@ -0,0 +1,64 @@
+package ru.aston.my_arraylist;
+
+import java.util.Arrays;
+
+/**
+ * Написать свой кастомный ArrayList, обязательно реализовать следующие методы:
+ * - add(int index, E element)
+ * - addAll(Collection<? extends E> c)
+ * - clear()
+ * - get(int index)
+ * - isEmpty()
+ * - remove(int index)
+ * - remove(Object o)
+ * - sort(Comparator<? super E> c)
+ *
+ * @param <T>
+ */
+public class MyArrayList<T> {
+    private T[] list;
+    private int size;
+    private static final int DEFAULT_CAPACITY = 10;
+    private int capacity = 10;
+
+    public MyArrayList(int capacity) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("Capacity <=0");
+        } else {
+            list = (T[]) new Object[capacity];
+        }
+    }
+
+    public MyArrayList() {
+        list = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
+
+    public void add(int index, T element) {
+        checkCapacity();
+        for (int i = size; i > index; i--) {
+            list[i] = list[i - 1];
+        }
+        list[index] = element;
+        size++;
+    }
+
+    private void checkCapacity() {
+        if (size == capacity) {
+            T[] newArray = (T[]) new Object[capacity * 2];
+            for (int i = 0; i < size; i++) {
+                newArray[i] = list[i];
+            }
+            list = newArray;
+            capacity *= 2;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "MyArrayList{" +
+               "list=" + Arrays.toString(list) +
+               ", size=" + size +
+               '}';
+    }
+}
