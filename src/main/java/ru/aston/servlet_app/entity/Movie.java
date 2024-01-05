db9a92478a8788914d30e5b8cae3c35c7ee65835
SemenenkoEugene <gks-08@mail.ru>
2024-01-05 21:00:10 +0300

    init: подкрутил Hibernate

--- src/main/java/ru/aston/servlet_app/entity/Movie.java
+++ src/main/java/ru/aston/servlet_app/entity/Movie.java
@@ -1,17 +1,52 @@
 package ru.aston.servlet_app.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
-@AllArgsConstructor
-@Builder
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString(exclude = "actors")
 @NoArgsConstructor
+@Entity
+@Table(name = "movie")
 public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "movie_id")
     private Integer id;
-    private Integer director;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "year_of_production")
     private Integer yearOfProduction;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.REFRESH, CascadeType.DETACH})
+    @JoinTable(
+            name = "actor_movie",
+            joinColumns = @JoinColumn(name = "movie_id"),
+            inverseJoinColumns = @JoinColumn(name = "actor_id")
+    )
+    private List<Actor> actors;
+
+    public void addActorToMovie(Actor actor) {
+        if (actors == null) {
+            actors = new ArrayList<>();
+        }
+        actors.add(actor);
+    }
+
+    public Movie(Integer id, String name, Integer yearOfProduction) {
+        this.id = id;
+        this.name = name;
+        this.yearOfProduction = yearOfProduction;
+    }
 }
